fe33210259a6472991f119d8aafb97e2bcd7be3c
Hari <shriharivaida099@gmail.com>
2024-05-01 02:01:25 -0700

    project_1 : Init commit contains first project

---
+++ pro_1/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="container">
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
